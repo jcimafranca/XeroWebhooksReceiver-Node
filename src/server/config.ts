c5b9a0ee3901f193c2077a790f80f1cbf4de1564
Phil Alsford <phil.alsford@xero.com>
2017-11-20 12:56:35 +1300

    Adds config so it will work in Heroku

---
+++ src/server/config.ts
@@ -0,0 +1,34 @@
+// Use your Partner App details and webhooks key from https://developer.xero.com/myapps
+
+export function getConfig(): IConfig {
+	if (process.env.CONSUMER_KEY) {
+		return {
+			userAgent: process.env.USER_AGENT,
+			consumerKey: process.env.CONSUMER_KEY,
+			consumerSecret: process.env.CONSUMER_SECRET,
+			authorizeCallbackUrl: process.env.AUTHORIZE_CALLBACK_URL,
+			privateKey: process.env.PRIVATE_KEY,
+			webhooksKey: process.env.WEBHOOKS_KEY
+		};
+	} else { return config; }
+}
+
+const config: IConfig = {
+	userAgent: 'Xero Node Webooks Sample App',
+	consumerKey: '',
+	consumerSecret: '',
+	authorizeCallbackUrl: 'http://localhost:8000/callback',
+	// tslint:disable-next-line:max-line-length
+	privateKey: ``,
+	webhooksKey: ''
+};
+
+export interface IConfig {
+	userAgent: string;
+	consumerKey: string;
+	consumerSecret: string;
+	authorizeCallbackUrl: string;
+	privateKeyPath?: string;
+	privateKey?: string | Buffer;
+	webhooksKey: string;
+}
